49eb668752e935abcd270de0e2c34c6450530433
Gary McHenry <gary.mchenry@att.net>
2017-08-15 19:43:03 -0500

    Savings Calculator

---
+++ main.js
@@ -0,0 +1,37 @@
+function calculate(){
+//console.log("calculate"); 
+var amount;
+var rate;
+var years;
+
+amount=parseFloat(document.getElementById("amount").value);
+rate=parseFloat(document.getElementById("rate").value);
+years=parseFloat(document.getElementById("years").value);
+
+
+var resultsTable=document.getElementById("results_table");
+
+var innerHTML = "";
+innerHTML += "<table>";
+innerHTML += "<tr><th>Month</th><th>Interest</th><th>Balance</th></tr>";
+
+for (var i=1; i<=years*12; i++){
+    var month = i;
+    var interest = amount * ((rate /100) / 12);
+    var balance = Math.round((amount + interest) * 100) / 100;
+    amount = balance;
+    
+    innerHTML +="<tr><td>" + month + "</td><td>" + interest.toFixed(2) + "</td><td>" + balance + "</td></tr>";
+}
+
+resultsTable.innerHTML = innerHTML;
+}
+
+function reset(){
+//console.log("reset");
+var resultsTable=document.getElementById("results_table");
+resultsTable.innerHTML = "";
+document.getElementById("amount").value="";
+document.getElementById("rate").value="";
+document.getElementById("years").value="";
+}
